ea0f7e66ee021139ed6a1b700ff79abd9af88de1
4zeddin <azeddinaitwaalla@gmail.com>
2024-10-13 14:30:22 +0100

    onboarding

--- lib/widget/content_model.dart
+++ lib/widget/content_model.dart
@@ -11,7 +11,7 @@ List<UnboardingContent> content = [
   UnboardingContent(
       title: "Pick your food from our menu\n", image: "images/screen1.png", description: "Select from our \n    Best Menu"),
   UnboardingContent(
-      title: "You can pay cash on Delevery\n       Card payement is available", image: "images/screen2.png", description: "Easy and Online payment"),
+      title: "You can pay cash on Delevery\n    Card payement is available", image: "images/screen2.png", description: "Easy and Online payment"),
   UnboardingContent(
-      title: "Deliver your food at your\n        Doortap", image: "images/screen3.png", description: "Quick Delevery at you Doorstap")
+      title: "Deliver your food at your\n              Doortap", image: "images/screen3.png", description: "Quick Delevery at you Doorstap")
 ];
